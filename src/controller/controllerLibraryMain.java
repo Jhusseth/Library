1025cf97cd2c3febda7bb569f6acbe3ca9e03cd9
plndrvlznrnj <paola.a.veloza@gmail.com>
2019-03-11 19:45:36 -0500

    I added the reader of interfaz

--- src/controller/controllerLibraryMain.java
+++ src/controller/controllerLibraryMain.java
@@ -1,5 +1,10 @@
 package controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -8,9 +13,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.stage.FileChooser;
 import javafx.stage.Window;
+import model.Library;
 
 public class controllerLibraryMain implements Initializable {
-
+	private Library library;
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
@@ -20,7 +26,7 @@ public class controllerLibraryMain implements Initializable {
 	public void chooseFile(ActionEvent e) {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open Resource File");
-		fileChooser.showOpenDialog(((Node) e.getSource()).getScene().getWindow());
+		File f = fileChooser.showOpenDialog(((Node) e.getSource()).getScene().getWindow());
 		
 	}
 	
--- src/model/Library.java
+++ src/model/Library.java
@@ -1,5 +1,40 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
 public class Library {
+
+	public void readFile(File file) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new FileReader(file));
+		int numberTest = Integer.parseInt(br.readLine());
+
+		for (int i = 0; i < numberTest; i++) {
+			int numberRecorderAvailable = Integer.parseInt(br.readLine());
+			int numberStands = Integer.parseInt(br.readLine());
+
+			for (int p = 0; p < numberStands ; p++) {
+				String standName = br.readLine();
+				// The first element is the Name of stand and second the numbers of books of this stand
+				String[] standOne = standName.split(" ");
+				for (int j = 0; j <= Integer.parseInt(standOne[1]); j++) {
+					// The first element is the ISBN, the second is the cost and the third is the amount of books availables
+					int[] dataInInt = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+				}
+			}
+			
+			int numOfClients = Integer.parseInt(br.readLine());
+			for(int j = 0; i < numOfClients;i++) {
+				//The first element in the array is the id of the client
+				//The next clientData.length-1 elements are theISNB of the book that the client buy
+				int[] clientData = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+			}
+			
+		}
+	}
 
 }
