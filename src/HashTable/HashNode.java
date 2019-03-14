e90e5edec41fc507a02e7c314394f9b61f9ef544
plndrvlznrnj <paola.a.veloza@gmail.com>
2019-03-13 21:36:48 -0500

    Hash added

---
+++ src/HashTable/HashNode.java
@@ -0,0 +1,30 @@
+package HashTable;
+
+public class HashNode<T, K> {
+
+	private T data;
+	
+	private K key;
+	
+	public HashNode(T data, K key){
+		this.data = data;
+		this.key = key;
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public K getKey() {
+		return key;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+
+	public void setKey(K key) {
+		this.key = key;
+	}
+
+}
--- src/HashTable/IHashTable.java
+++ src/HashTable/IHashTable.java
@@ -1,11 +1,36 @@
+//package HashVable;
+//
+//public interface IHashVable<K,V> {
+//	
+//	K getIndex(K key);
+//	V insertKey(K key, V date);
+//	boolean revome(K key);
+//	V get(K key);
+//	
+//
+//}
 package HashTable;
 
 public interface IHashTable<K,V> {
 	
-	K getIndex(K key);
-	V insertKey(K key, V date);
-	boolean revome(K key);
-	V get(K key);
+//	K getIndex(K key);
+//	V insertKey(K key, V date);
+//	boolean revome(K key);
+//	V get(K key);
+	
+	public void put(K key,V value);
+	
+	public V get(K key);
+	
+	public V remove(K key);
+	
+	public boolean isEmpty();
+	
+	public int tableLength();
+	
+	public int hash(K key);
+	
+	public void rehashing();
 	
 
 }
