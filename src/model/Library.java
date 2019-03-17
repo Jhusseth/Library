4677836d6cb99c5ece0e23786d025c8e1fd9faea
plndrvlznrnj <paola.a.veloza@gmail.com>
2019-03-17 17:41:14 -0500

    Added other things

--- src/model/Library.java
+++ src/model/Library.java
@@ -33,59 +33,7 @@ public class Library {
 
         br = new BufferedReader(new FileReader(file));
         int numberTest = Integer.parseInt(br.readLine());
-        for (int i = 0; i < numberTest; i++) {
-            numberRecorderAvailable = Integer.parseInt(br.readLine());
-            recoders = new Recorder[numberRecorderAvailable];
-            for (int j = 0; j < numberRecorderAvailable; j++) {
-                recoders[j] = new Recorder();
-            }
-
-            int numberStands = Integer.parseInt(br.readLine());
-            shelvings = new HashTable<>(numberStands*10);
-
-            System.out.println("Shelving before clients:");
-
-            for (int p = 0; p < numberStands ; p++) {
-                // The first element is the Name of stand and second the numbers of books of this stand
-                String[] shelving = br.readLine().split(" ");
-                shelvings.put(shelving[0],new Shelving(shelving[0]));
-                for (int j = 0; j < Integer.parseInt(shelving[1]); j++) {
-                    // The first element is the ISBN, the second is the cost and the third is the amount of books availables
-                    int[] dataInInt = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                    shelvings.get(shelving[0]).addBook(new Book(dataInInt[0],dataInInt[1]),dataInInt[2]);
-                    shelvings.put(""+dataInInt[0],shelvings.get(shelving[0]));
-                }
-                System.out.println(shelvings.get(shelving[0]));
-            }
-
-            System.out.println("Clients:");
-
-            int numOfClients = Integer.parseInt(br.readLine());
-            clients = new Client[numOfClients];
-            for(int j = 0; j < numOfClients;j++) {
-                //The first element in the array is the id of the client
-                //The next clientData.length-1 elements are theISNB of the book that the client buy
-                int[] clientData = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                clients[j] = new Client(clientData[0],j+1);
-                for (int k = 1; k < clientData.length; k++) {
-                    clients[j].addBook(shelvings.get(""+clientData[k]).getBook(clientData[k]));
-                }
-                System.out.println(clients[j]);
-            }
-            Arrays.sort(clients);
-            System.out.println("\nClients to Recorders");
-            for (Client c: clients) {
-                System.out.println(c);
-            }
-            recorders();
-
-            System.out.println("\nFinal Stage");
-            for (Client c: clients) {
-                System.out.println(c);
-            }
-
-        }
-        
+        initRecorders();
     }
 
     public String toPayQueue() {
@@ -141,7 +89,7 @@ public class Library {
         return Stands;
     }
 
-    public void iniciarCajeros() throws IOException {
+    public void initRecorders() throws IOException {
         numberRecorderAvailable = Integer.parseInt(br.readLine());
         recoders = new Recorder[numberRecorderAvailable];
         for (int j = 0; j < numberRecorderAvailable; j++) {
